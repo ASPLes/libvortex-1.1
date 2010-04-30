9867f632a031319d1d54d9b8072df55aafb1dc49
Francis Brosnan Blázquez <francis@aspl.es>
2010-04-30 15:41:37 +0000

    libexarg: * [fix] Fixed libexarg to use axl definitions if present to   detect vasprintf and win32 platforms. Updated libvortex-1.1   and turbulence code.

--- xml-rpc-gen/exarg.h
+++ xml-rpc-gen/exarg.h
@@ -110,9 +110,9 @@ typedef struct _ExArgument ExArgument;
 void         exarg_parse        (int         argc,
 			       char     ** argv);
 
-void         exarg_end          ();
+void         exarg_end          (void);
 
-void         exarg_disable_help ();
+void         exarg_disable_help (void);
 
 void         exarg_add_usage_header  (char * header);
 
@@ -140,7 +140,9 @@ void         exarg_set_obligatory   (char * arg_name);
 void         exarg_accept_free_args (int accept);
 
 void         exarg_define           (char * arg_name,
-				   char * value);
+				     char * value);
+
+void         exarg_undef            (char * arg_name);
 
 int          exarg_is_defined       (char     * arg_name);
 
@@ -152,13 +154,13 @@ char       * exarg_get_string_alloc (char * arg_name);
 
 int          exarg_get_int          (char * arg_name);
 
-ExArgument * exarg_get_params       ();
+ExArgument * exarg_get_params       (void);
 
 const char * exarg_param_get        (ExArgument * arg);
 
 ExArgument * exarg_param_next       (ExArgument * arg);
 
-int          exarg_get_params_num   ();
+int          exarg_get_params_num   (void);
 
 #endif
 
