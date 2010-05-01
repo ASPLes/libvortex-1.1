bb23d6de769eb7193e6126d7abaf7ef50d5b97db
Francis Brosnan Blázquez <francis@aspl.es>
2010-05-01 14:08:27 +0000

    turbulence, libexarg: * [fix] Updates in configure.ac to better detect vasprintf function when   available.

--- py-vortex/doc/conf.py
+++ py-vortex/doc/conf.py
@@ -42,9 +42,9 @@ copyright = '2009, Advanced Software Production Line, S.L.'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.1.4.b4333.g4333'
+version = '1.1.4.b4338.g4343'
 # The full version, including alpha/beta/rc tags.
-release = '1.1.4.b4333.g4333'
+release = '1.1.4.b4338.g4343'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
