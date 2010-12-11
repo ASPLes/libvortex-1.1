0cbfc13a3de9413909825b878c0c88a79086c7c5
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2010-12-11 21:57:41 +0000

    libvortex-1.1: * [fix] Removing all references to VortexCBuffer.

--- trunk/libvortex-1.1/test/vortex-file-transfer-server.c
+++ trunk/libvortex-1.1/test/vortex-file-transfer-server.c
@@ -167,10 +167,6 @@ void frame_received_with_feeder (VortexChannel    * channel,
 	/* create the feeder */
 	feeder = vortex_payload_feeder_file (ctx, FILE_TO_TRANSFER, axl_false);
 
-	/* set a buffer */
-	printf ("Configuring 64k buffer..\n"); 
-	vortex_payload_feeder_file_set_buffer (feeder, 65536); 
-
 	/* send rpy */
 	if (! vortex_channel_send_rpy_from_feeder (channel, feeder, vortex_frame_get_msgno (frame))) {
 		printf ("ERROR: failed to send RPY using feeder..\n");
