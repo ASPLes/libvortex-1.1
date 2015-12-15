094faae677f818c2bf82415963f9be3336122a84
Francis Brosnan Blázquez <francis@aspl.es>
2015-12-15 17:00:01 +0100

    libvortex-1.1: * [fix] Updated freebsd build packages to support more archs

--- freebsd/buildpackages.sh
+++ freebsd/buildpackages.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/libvortex-1.1 /usr/src/libvortex-1.1/freebsd/9-x86-64/ --skip-build --version=`cat /usr/src/libvortex-1.1/VERSION` --description="BEEP core implementation written in C" --outdir=/usr/src/freebsd-packages
+# Argument to this script should be one of the folders
+# in this directory
+
+python /usr/src/freebsd-pkg-builder/pkg-builder.py /usr/src/libvortex-1.1 /usr/src/libvortex-1.1/freebsd/arch/ --skip-build --version=`cat /usr/src/libvortex-1.1/VERSION` --description="BEEP core implementation written in C" --outdir=/usr/src/freebsd-packages
 
